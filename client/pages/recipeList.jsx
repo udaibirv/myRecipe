bd45481675996aba26309a3372be1f255eefd8c0
Udaibir Virk <udaibirv@gmail.com>
2021-02-10 00:32:02 +0000

    set up recipeList form component. #2

---
+++ client/pages/recipeList.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import recipeForm from './recipeForm';
+
+class recipeList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: []
+    };
+
+  }
+}
