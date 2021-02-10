55e35888ba4d1b36533d3f540368d7a8e56dcc15
Udaibir Virk <udaibirv@gmail.com>
2021-02-10 01:11:21 +0000

    set up state with recipes set to an empty array. Create a componenetDidMount. #2

--- client/pages/recipeList.jsx
+++ client/pages/recipeList.jsx
@@ -1,12 +1,25 @@
-import react from 'react';
-import recipeForm from './recipeForm';
+import { response } from 'express';
+import React from 'react';
+import RecipeForm from './recipeForm';
 
-class recipeList extends React.Component {
+class RecipeList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       recipes: []
     };
 
+  }
+
+  componentDidMount() {
+    this.getAllRecipes();
+  }
+
+  getAllRecipes() {
+    fetch('api/recipes/')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ recipes: data });
+      });
   }
 }
