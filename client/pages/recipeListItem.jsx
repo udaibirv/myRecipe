30281b302624841a74d4d3b7f033a5485516806b
udaibirv <udaibirv@gmail.com>
2021-02-13 17:24:05 -0800

    completed view-recipes feature

--- client/pages/recipeListItem.jsx
+++ client/pages/recipeListItem.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 
-const styles = {
-  recipe: {
-    display: 'block',
-    cursor: 'pointer'
-  }
-};
-
 export default class RecipeListItem extends React.Component {
 
   render() {
@@ -20,38 +13,8 @@ export default class RecipeListItem extends React.Component {
                   <p className="card-text"> {`Equipment Needed: ${this.props.recipe.equipment}`}</p>
                 </div>
 
-    {/* <div className="container">
-      <div className="row">
-        <div className="col-sm-4">
-          <div className="card-deck">
-              <div className="card">
-                <div className="card-body">
-                  <h4 className="card-title">{this.props.recipe.recipeName}</h4>
-                  <p className="card-text">{this.props.recipe.recipeOrigin}</p>
-                </div>
-                <ul className="list-group">
-                  <li className="list-group-item"> {`Equipment Needed: ${this.props.recipe.equipment}`}</li>
-                </ul>
-              </div>
-          </div>
-        </div>
-      </div>
-    </div> */}
-      {/* <div className="card mb-3" >
-        <div className="card-body">
-          <h4 className="card-title">{this.props.recipe.recipeName}</h4>
-          <p className="card-text">{this.props.recipe.recipeOrigin}</p>
-        </div>
-        <ul className="list-group">
-          <li className="list-group-item"> {`Equipment Needed: ${this.props.recipe.equipment}`}</li>
-        </ul>
-      </div> */}
       </a>
     );
   }
 
 }
-
-{ /* <li>
-  {this.props.recipe.recipeName}
-</li>; */ }
