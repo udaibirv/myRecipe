a6cdd72b1b336c1e1990d3663b6acfa7e806a032
udaibirv <udaibirv@gmail.com>
2021-02-25 16:42:17 -0800

    fixed ssl setting

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const pg = require('pg');
 const db = new pg.Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 const app = express();
