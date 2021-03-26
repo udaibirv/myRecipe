083ffecdd7af9159454c7ad0915d4a342203787f
udaibirv <udaibirv@gmail.com>
2021-03-26 16:32:01 -0700

    set up app context and began building out the sign in and sign up form for user login

---
+++ client/pages/auth-form.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default class AuthForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { action } = this.props;
+    const requestBody = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    };
+  }
+}
